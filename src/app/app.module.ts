d2898bfc352c82d578e655f25a1f307605e3007d
Huguez <carlos.huguez@gmail.com>
2020-10-07 12:55:03 -0700

    se creo la pagina dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NoPageFounfComponent } from './pages/no-page-founf/no-page-founf.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    NoPageFounfComponent
+    NoPageFounfComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
