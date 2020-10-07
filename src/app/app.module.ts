dfbfc55699c0f1be1459e4f388a2258545194fa6
Huguez <carlos.huguez@gmail.com>
2020-10-07 12:53:46 -0700

    se creo el folder de paginas y la pagina 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NoPageFounfComponent } from './pages/no-page-founf/no-page-founf.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NoPageFounfComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/no-page-founf/no-page-founf.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-page-founf',
+  templateUrl: './no-page-founf.component.html',
+  styles: [
+  ]
+})
+export class NoPageFounfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
