850232499e274ab989653aa553923ec6b2bce421
Huguez <carlos.huguez@gmail.com>
2020-10-09 14:58:16 -0700

    Rutas hijas - ForChild()

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from "@angular/router";
+import { PagesRoutingModule } from './pages/pages.routing';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { GraficaComponent } from './pages/grafica/grafica.component';
+
 import { NoPageFoundComponent } from './404/no-page-found.component';
-import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
-  {
-    path: '', 
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: GraficaComponent },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ]
-  },
   
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
@@ -29,7 +17,10 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [],
-  imports: [ RouterModule.forRoot( routes ) ],
+  imports: [ 
+    RouterModule.forRoot( routes ),
+    PagesRoutingModule
+  ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
