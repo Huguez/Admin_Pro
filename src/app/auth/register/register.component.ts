2d7ebba13db13a7302601797161c4d8d452ca152
Huguez <carlos.huguez@gmail.com>
2020-10-28 16:57:03 -0700

    validanciones del formulario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -7,19 +7,37 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: [ './register.component.css' ]
 })
 export class RegisterComponent {
-
   
+  public formSumitted = false;
+
   public registerForm = this.fb.group({
     nombre:    ['user', [ Validators.required ] ],
-    email:     ['example@email.com', [Validators.required, ] ],
-    password:  ['password', [Validators.required ] ],
-    password2: ['password', [Validators.required ] ],
+    email:     ['user@example.com', [Validators.required, Validators.email ] ],
+    password:  ['1234', [Validators.required ] ],
+    password2: ['1234', [Validators.required ] ],
     terminos:  [ false, [ Validators.required ] ]
   });
 
   constructor( private fb: FormBuilder ){}
   
   crearUsuario(){
+    this.formSumitted = true;
     console.log( this.registerForm.value );
+    if( this.registerForm.valid ){
+      console.log("posteando formulario...");
+    }else{
+      console.log("formulario no es correcto");
+    }
+  }
+
+  campoNoValido( campo: string ){
+    if( !this.registerForm.get( campo ).valid  && this.formSumitted ){
+      return true;  
+    }
+    return false;
+  }
+
+  aceptarTerminos(  ){
+    return !this.registerForm.get('terminos').value && this.formSumitted;
   }
 }
