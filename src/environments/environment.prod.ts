3710eb3bc1154e1931010428c266507342aa31fa
Huguez <carlos.huguez@gmail.com>
2021-01-12 22:17:07 -0700

    adaptacion con urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  base_url: 'http://localhost:3005/api'
-
+  // base_url: 'http://localhost:3005/api'
+  // base_url: 'https://adminbackend1.herokuapp.com'
+  base_url: 'https://adminbackend1.herokuapp.com/api'
 };
